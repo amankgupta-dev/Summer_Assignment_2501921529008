666330476990c6216f8c6c8c60c0ea4a954f5d35
Aman Kumar Gupta <amangupta190203@gmail.com>
2026-06-13 22:06:46 +0530

    Update MoveZeroes.java

--- Week_1/Day_3/MoveZeroes.java
+++ Week_1/Day_3/MoveZeroes.java
@@ -1,1 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
 
+        // Pehle saare non-zero elements aage le aao
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        // Baaki jagah 0 fill kar do
+        while (index < nums.length) {
+            nums[index] = 0;
+            index++;
+        }
+    }
+}
