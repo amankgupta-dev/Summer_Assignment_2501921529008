857b992449959ff550b0a282b7eb1d7ad95ffb00
Aman Kumar Gupta <amangupta190203@gmail.com>
2026-06-18 16:39:59 +0530

    Create RansomNote.java

---
+++ Week_2/Day_1/RansomNote.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] count = new int[26];
+
+        // Count characters in magazine
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++;
+        }
+
+        // Use characters for ransomNote
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (count[ransomNote.charAt(i) - 'a'] == 0) {
+                return false;
+            }
+            count[ransomNote.charAt(i) - 'a']--;
+        }
+
+        return true;
+    }
+}
