471dcd4c722424dd517fb0b10f97631c5d695d2f
Aman Kumar Gupta <amangupta190203@gmail.com>
2026-07-07 23:13:11 +0530

    Create ToTestInt.java

---
+++ Week_4/Opps_Problem/ToTestInt.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ToTestInt {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        Arithmetic obj = new Arithmetic();
+
+        System.out.println("Square = " + obj.square(n));
+    }
+}
